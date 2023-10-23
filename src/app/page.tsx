8a9784c698f3961382d25da05e645bf294fa1e01
Noma <wiostz98kr@gmail.com>
2023-10-23 20:34:26 +0900

    Server/Client Component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,16 @@
+//Server Component
+
+import React from 'react';
+import os from 'os'; //Node APIs
+import Counter from '../components/Counter';
+
 export default function Home() {
-  return <h1>Home page</h1>;
+  console.log(os.hostname());
+  console.log('안녕 - 서버');
+  return (
+    <>
+      <h1>Home page</h1>;
+      <Counter />
+    </>
+  );
 }
---
+++ src/components/Counter.tsx
@@ -0,0 +1,20 @@
+//Client Component
+
+'use client';
+
+import React, { useState } from 'react';
+
+function Counter() {
+  const [count, setCount] = useState(0);
+  console.log('안녕 - 클라이언트');
+  return (
+    <>
+      <p>{count}</p>
+      <button onClick={() => setCount((num) => num + 1)}>
+        숫자 증가 시키기
+      </button>
+    </>
+  );
+}
+
+export default Counter;
