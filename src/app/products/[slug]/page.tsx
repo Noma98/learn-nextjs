9068bd9ec286e0e7e435f6ea390d2a86c0f07ce1
Noma <wiostz98kr@gmail.com>
2023-10-24 18:36:51 +0900

    Fetch data from server file

--- src/app/products/[slug]/page.tsx
+++ src/app/products/[slug]/page.tsx
@@ -12,18 +12,16 @@ export function generateMetadata({ params }: Props) {
     title: `제품의 이름: ${params.slug}`,
   };
 }
-export default function Pants({ params: { slug } }: Props) {
-  //서버 파일에 있는 데이터 중 해당 제품의 정보를 찾아서 보여주기
-  const product = getProduct(slug);
+export default async function ProductPage({ params: { slug } }: Props) {
+  const product = await getProduct(slug);
   if (!product) {
     notFound();
   }
-  return <h1>{product} 제품 안내 페이지</h1>;
+  return <h1>{product.name} 제품 안내 페이지</h1>;
 }
-export function generateStaticParams() {
-  //모든 제품의 페이지들을 미리 만들어 둘 수 있게 할 예정(SSG)
-  const products = getProducts();
+export async function generateStaticParams() {
+  const products = await getProducts();
   return products.map((product) => ({
-    slug: product,
+    slug: product.id,
   }));
 }
