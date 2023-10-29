1920e93c3e010902a6fb6cab190fd36f098ff059
Noma <wiostz98kr@gmail.com>
2023-10-29 14:01:06 +0900

    SSR(v12_getServerSideProps)

---
+++ src/pages/ssr.tsx
@@ -0,0 +1,30 @@
+import { getProducts } from '@/service/products';
+import Link from 'next/link';
+import React from 'react';
+import MeowArticle from '@/components/MeowArticle';
+
+export default async function ProductsPage() {
+  const products = await getProducts();
+
+  return (
+    <>
+      <h1>Products Page</h1>
+      <ul>
+        {products.map(({ id, name }, index) => (
+          <li key={index}>
+            <Link href={`/products/${id}`}>{name}</Link>
+          </li>
+        ))}
+      </ul>
+      <MeowArticle />
+    </>
+  );
+}
+//SSR
+export async function getServerSideProps() {
+  const products = await getProducts();
+  return {
+    props: { products },
+    revalidate: 10,
+  };
+}
