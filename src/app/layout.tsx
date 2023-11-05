35901c734679f791842a4b4acdedabd9822e987b
Noma <wiostz98kr@gmail.com>
2023-11-05 13:52:38 +0900

    Font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,11 @@ import React from 'react';
 import { Metadata } from 'next';
 import Link from 'next/link';
 import styles from './layout.module.css';
+import { Open_Sans } from 'next/font/google';
+import { Nanum_Gothic } from 'next/font/google'; //variable font가 아님
 
+const sans = Open_Sans({ subsets: ['latin'] });
+const gothic = Nanum_Gothic({ weight: '700', subsets: ['latin'] });
 export const metadata: Metadata = {
   title: '제품 사이트',
   description: '제품을 판매하는 곳입니다.',
@@ -16,10 +20,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang='en'>
+    <html lang='en' className={sans.className}>
       <body className={styles.body}>
         <header className={styles.header}>
-          <h1>Demo Note</h1>
+          <h1 className={gothic.className}>Demo Note</h1>
           <nav>
             <Link href='/products'>Products</Link>
             <Link href='/about'>About</Link>
